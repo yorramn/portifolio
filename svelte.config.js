50ba55daec5130fb68f34cf9512da3b6e73ec1f3
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-11-21 15:59:09 +0100

    ci: add gh workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const base = '/slick-portfolio-svelte-5';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -11,7 +13,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? base : ''
+		}
 	}
 };
 
