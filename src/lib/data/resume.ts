e653d1c33b45199746da886f4003bf400e1749ef
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-11-21 09:42:15 +0100

    feat: add resume page

---
+++ src/lib/data/resume.ts
@@ -0,0 +1,7 @@
+const title = 'Resumé';
+
+const resume = '/pdf/resume.pdf';
+
+const ResumeData = { title, resume };
+
+export default ResumeData;
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TitledPage from '$lib/components/common/titled-page/titled-page.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import ResumeData from '$lib/data/resume';
+</script>
+
+<TitledPage title={ResumeData.title}>
+	<a href={ResumeData.resume} class="mx-auto">
+		<Button>Download</Button>
+	</a>
+	<iframe src={ResumeData.resume} class="h-full w-full" title={ResumeData.title}></iframe>
+</TitledPage>
