9c3dda882de668ccf25f43e05332a71f83d4f932
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-11-21 15:48:35 +0100

    fix: nav-bar not responsive

--- src/lib/components/common/nav-bar/nav-bar.svelte
+++ src/lib/components/common/nav-bar/nav-bar.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
+	import {
+		Dialog,
+		DialogClose,
+		DialogContent,
+		DialogFooter,
+		DialogTrigger
+	} from '$lib/components/ui/dialog';
 	import Icon from '$lib/components/ui/icon/icon.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import { Tooltip, TooltipTrigger } from '$lib/components/ui/tooltip';
 	import TooltipContent from '$lib/components/ui/tooltip/tooltip-content.svelte';
 	import H4 from '$lib/components/ui/typography/h4.svelte';
+	import Large from '$lib/components/ui/typography/large.svelte';
+	import BaseData from '$lib/data/base';
 	import NavBarData from '$lib/data/nav-bar';
-	import { toggleMode, mode } from 'mode-watcher';
+	import { mode, toggleMode } from 'mode-watcher';
+
+	let isDarkMode = $derived($mode === 'dark');
 </script>
 
 <div
-	class="border-1 absolute left-0 right-0 top-0 z-10 flex h-[50px] flex-row items-center border-b bg-[--bg] px-8 backdrop-blur-xl"
+	class="border-1 absolute left-0 right-0 top-0 z-10 flex h-[50px] flex-row items-center border-b bg-[--bg] px-4 backdrop-blur-xl sm:px-8"
 	style="--bg : hsl(var(--background) / 0.5)"
 >
 	<div class="sm:flex-1">
@@ -25,7 +37,9 @@
 			<H4 className="hidden lg:block">{NavBarData.left.title}</H4>
 		</a>
 	</div>
-	<div class="flex flex-[2] flex-row items-center justify-center gap-2">
+
+	<!-- larger than sm -->
+	<div class="hidden flex-[2] flex-row items-center justify-center gap-2 sm:flex">
 		{#each NavBarData.items as item}
 			<a href={item.href}>
 				<Tooltip>
@@ -42,14 +56,70 @@
 			</a>
 		{/each}
 	</div>
-	<div class="flex flex-row items-center justify-end gap-2 sm:flex-1">
+	<div class="hidden flex-row items-center justify-end gap-2 sm:flex sm:flex-1">
 		<a href="/search">
 			<Button variant="ghost" class="text-xl">
 				<Icon icon="i-carbon-search" />
 			</Button>
 		</a>
 		<Button variant="ghost" class="text-xl" on:click={toggleMode}>
-			<Icon icon={$mode === 'dark' ? 'i-carbon-moon' : 'i-carbon-sun'} />
+			<Icon icon={isDarkMode ? 'i-carbon-moon' : 'i-carbon-sun'} />
 		</Button>
+	</div>
+
+	<!-- sm -->
+	<div class="flex flex-[2] flex-row items-center justify-center sm:hidden">
+		<a href="/">
+			<Large>{BaseData.fullName}</Large>
+		</a>
+	</div>
+	<div class="flex flex-row items-center justify-center sm:hidden">
+		<Dialog>
+			<DialogTrigger>
+				<Button size="icon" variant="ghost">
+					<Icon className="text-xl" icon="i-carbon-menu" />
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<div class="flex flex-col gap-2 pt-4">
+					{#each NavBarData.items as item}
+						<DialogClose>
+							<a href={item.href} class="w-full">
+								<Button
+									class="flex w-full flex-row items-center justify-start gap-2"
+									variant="ghost"
+								>
+									<Icon icon={item.icon} className="text-xl" />
+									<div>{item.title}</div>
+								</Button>
+							</a>
+						</DialogClose>
+					{/each}
+					<Separator />
+					<DialogClose>
+						<a href={'/search'} class="w-full">
+							<Button class="flex w-full flex-row items-center justify-start gap-2" variant="ghost">
+								<Icon icon={'i-carbon-search'} className="text-xl" />
+								<div>Search</div>
+							</Button>
+						</a>
+					</DialogClose>
+					<Separator />
+					<Button
+						class="flex w-full flex-row items-center justify-start gap-2"
+						variant="ghost"
+						on:click={toggleMode}
+					>
+						<Icon icon={isDarkMode ? 'i-carbon-moon' : 'i-carbon-sun'} className="text-xl" />
+						<div>{isDarkMode ? 'Dark' : 'Light'}</div>
+					</Button>
+				</div>
+				<DialogFooter class="items-end">
+					<DialogClose>
+						<Button>Close</Button>
+					</DialogClose>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	</div>
 </div>
